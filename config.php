f0295a64fd3546d3e7032ec94c75506d3a1468ed
Mohd Haji <mohdhaji24@gmail.com>
2017-03-13 18:34:29 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //Connection to database
  
- $db = mysqli_connect("127.0.0.1","root","","project")
+ $db = mysqli_connect("127.0.0.1","test","test","project")
  or die('Error connecting to MySQL server.');
 ?>
