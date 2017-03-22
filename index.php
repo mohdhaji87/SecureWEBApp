f56e4d399cea88f5df5304f49b33c2e379c09a2e
Mohd Haji <mohdhaji24@gmail.com>
2017-03-22 21:39:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ header("X-Frame-Options: DENY");
 ?>
 
 <body>
-<h1> Registration form</h1>
+  
+  <h1> Secure WEB Application </h1>
+  
+<h2> Registration form</h2>
 <form action="register.php" method="POST">
 
 Username: <input type="text" name="username" value=""> </br>
@@ -14,7 +17,7 @@ Email: <input type="email" name="email" value=""></br>
 Gender : <input type="radio" name="gender" value="male"> Male <input type="radio" name="gender" value="female"> Female
 <input type="submit" name="register" value="register">
 </form>
-<h1> Login form</h1>
+<h2> Login form</h2>
 <form action="login.php" method="POST">
 
 Username: <input type="text" name="username" value=""> </br>
